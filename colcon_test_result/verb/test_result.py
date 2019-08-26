13302551927f3fda70aa9be0474fdbb3f8e93e44
Dan Rose <dan@digilabs.io>
2019-08-26 17:10:06 -0500

    Address code review feedback

--- colcon_test_result/verb/test_result.py
+++ colcon_test_result/verb/test_result.py
@@ -47,8 +47,8 @@ class TestResultVerb(VerbExtensionPoint):
             context.args.test_result_base,
             collect_details=context.args.verbose))
 
+        results = [r for r in results if r.error_count or r.failure_count or context.args.all]
         results.sort(key=lambda r: r.path)
-        results = [r for r in results if r.failure_count or context.args.all]
 
         if context.args.result_files_only:
             for result in results:
